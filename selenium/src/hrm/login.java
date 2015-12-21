238027931e06e5e8ba007daf63b2d67e733ab68c
durga <bhavani3217@gmail.com>
2015-12-21 16:23:25 +0530

    LoginTest

---
+++ selenium/src/hrm/login.java
@@ -0,0 +1,20 @@
+package hrm;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class login 
+{
+	@Test
+	public void LoginTest()
+	{
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://opensource.demo.orangehrm.com/");
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin");
+		driver.findElement(By.id("btnLogin")).click();
+	}
+
+}
